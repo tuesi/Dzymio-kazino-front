4b1f3102750f4d46bc90b508b6fdc617e09513d3
Adomaitis Tadas <tadas.adomaitis@mif.stud.vu.lt>
2022-11-24 19:09:01 +0200

    nu please

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   backendUrl: "https://dzymio-kazino-back-production.up.railway.app"
 };
 
